2ed49c65135556e10ece60ea62a795464f2998a7
Hellsing666 <subhajitmondal246@gmail.com>
2026-02-16 20:08:11 +0530

    chore: configure app for Render deployment

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const API_URL = process.env.REACT_APP_API_URL;
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
